4dcd9956b6803c36d7b8eb9202eed3c95c33fb80
Robert Eggar <roberteggar@gmail.com>
2016-10-21 20:42:56 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,71 @@
+# AutoValue: Ignore Hash Equals Extension [![Build Status](https://travis-ci.org/REggar/auto-value-ignore-hash-equals.svg?branch=master)](https://travis-ci.org/REggar/auto-value-ignore-hash-equals)
+An extension for Google's [AutoValue](https://github.com/google/auto/tree/master/value) that omits
+`@IgnoreHashEquals` field values from `hashCode()` and `equals()`.
+
+
+## Usage
+
+Include the extension in your project, define a `@IgnoreHashEquals` annotation, and apply it to any
+fields that you wish to be ignored from the generated `hashCode` and `equals` implementation.
+
+```java
+@Retention(SOURCE)
+@Target({METHOD, PARAMETER, FIELD})
+public @interface IgnoreHashEquals {
+}
+```
+
+```java
+@AutoValue
+public abstract class User {
+  public abstract String name();
+  @IgnoreHashEquals public abstract String location();
+}
+```
+
+When you call `hashCode()` or `equals()` any `@IgnoreHashEquals` properties will be ignored from the
+calculation.
+
+
+## Download
+
+Add a Gradle dependency:
+
+```groovy
+apt 'com.github.reggar:auto-value-ignore-hash-equals:1.0.0'
+```
+(Using the [android-apt](https://bitbucket.org/hvisser/android-apt) plugin)
+
+or Maven:
+```xml
+<dependency>
+  <groupId>com.github.reggar</groupId>
+  <artifactId>auto-value-ignore-hash-equals</artifactId>
+  <version>1.0.0</version>
+  <scope>provided</scope>
+</dependency>
+```
+
+Snapshots of the development version are available in [Sonatype's `snapshots` repository][snap].
+
+## Notes
+
+This library is heavily inspired by Square's [AutoValue: Redacted Extension](https://github.com/square/auto-value-redacted).
+
+## License
+
+```
+Copyright 2016 Robert Eggar.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+```
---
+++ src/test/java/com/github/reggar/ignorehashequals/AutoValueIgnoreHashEqualsExtensionTest.java
@@ -0,0 +1,139 @@
+/*
+ * Copyright (C) 2016 Robert Eggar.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.reggar.ignorehashequals;
+
+import com.google.auto.value.processor.AutoValueProcessor;
+import com.google.testing.compile.JavaFileObjects;
+import java.util.Arrays;
+import javax.tools.JavaFileObject;
+import org.junit.Before;
+import org.junit.Test;
+
+import static com.google.common.truth.Truth.assertAbout;
+import static com.google.testing.compile.JavaSourcesSubjectFactory.javaSources;
+
+public final class AutoValueIgnoreHashEqualsExtensionTest {
+  private JavaFileObject ignoreHashEquals;
+  private JavaFileObject nullable;
+
+  @Before public void setUp() {
+    ignoreHashEquals = JavaFileObjects.forSourceString("test.IgnoreHashEquals", ""
+        + "package test;\n"
+        + "import java.lang.annotation.Retention;\n"
+        + "import java.lang.annotation.Target;\n"
+        + "import static java.lang.annotation.ElementType.FIELD;\n"
+        + "import static java.lang.annotation.ElementType.METHOD;\n"
+        + "import static java.lang.annotation.ElementType.PARAMETER;\n"
+        + "import static java.lang.annotation.RetentionPolicy.SOURCE;\n"
+        + "@Retention(SOURCE)\n"
+        + "@Target({METHOD, PARAMETER, FIELD})\n"
+        + "public @interface IgnoreHashEquals {\n"
+        + "}");
+    nullable = JavaFileObjects.forSourceString("test.Nullable", ""
+        + "package test;\n"
+        + "import java.lang.annotation.Retention;\n"
+        + "import java.lang.annotation.Target;\n"
+        + "import static java.lang.annotation.ElementType.FIELD;\n"
+        + "import static java.lang.annotation.ElementType.METHOD;\n"
+        + "import static java.lang.annotation.ElementType.PARAMETER;\n"
+        + "import static java.lang.annotation.RetentionPolicy.CLASS;\n"
+        + "@Retention(CLASS)\n"
+        + "@Target({METHOD, PARAMETER, FIELD})\n"
+        + "public @interface Nullable {\n"
+        + "}");
+  }
+
+  @Test public void simple() {
+    JavaFileObject source = JavaFileObjects.forSourceString("test.Test", ""
+            + "package test;\n"
+            + "import com.google.auto.value.AutoValue;\n"
+            + "@AutoValue public abstract class Test {\n"
+            + "@IgnoreHashEquals public abstract String a();\n"
+            + "public abstract int b();\n"
+            + "public abstract long c();\n"
+            + "public abstract float d();\n"
+            + "public abstract double e();\n"
+            + "public abstract boolean f();\n"
+            + "public abstract int[] g();\n"
+            + "public abstract String h();\n"
+            + "@Nullable public abstract String i();\n"
+            + "}\n"
+    );
+
+    JavaFileObject expectedSource = JavaFileObjects.forSourceString("test/AutoValue_Test", ""
+                + "package test;\n"
+                + "\n"
+                + "import java.lang.Object;\n"
+                + "import java.lang.Override;\n"
+                + "import java.lang.String;\n"
+                + "import java.util.Arrays;\n"
+                + "\n"
+                + "final class AutoValue_Test extends $AutoValue_Test {\n"
+                + "  AutoValue_Test(String a, int b, long c, float d, double e, boolean f, int[] g, String h, String i) {\n"
+                + "    super(a, b, c, d, e, f, g, h, i);\n"
+                + "  }\n"
+                + "\n"
+                + "  @Override\n"
+                + "  public final boolean equals(Object o) {\n"
+                + "    if (o == this) {\n"
+                + "      return true;\n"
+                + "    }\n"
+                + "    if (o instanceof Test) {\n"
+                + "      Test that = (Test) o;\n"
+                + "      return (this.b() == that.b())\n"
+                + "          && (this.c() == that.c())\n"
+                + "          && (Float.floatToIntBits(this.d()) == Float.floatToIntBits(that.d()))\n"
+                + "          && (Double.doubleToLongBits(this.e()) == Double.doubleToLongBits(that.e()))\n"
+                + "          && (this.f() == that.f())\n"
+                + "          && (Arrays.equals(this.g(), that.g()))\n"
+                + "          && (this.h().equals(that.h()))\n"
+                + "          && ((this.i() == null) ? (that.i() == null) : this.i().equals(that.i()));\n"
+                + "    }\n"
+                + "    return false;\n"
+                + "  }\n"
+                + "\n"
+                + "  @Override\n"
+                + "  public final int hashCode() {\n"
+                + "    int h = 1;\n"
+                + "    h *= 1000003;\n"
+                + "    h ^= this.b();\n"
+                + "    h *= 1000003;\n"
+                + "    h ^= (this.c() >>> 32) ^ this.c();\n"
+                + "    h *= 1000003;\n"
+                + "    h ^= Float.floatToIntBits(this.d());\n"
+                + "    h *= 1000003;\n"
+                + "    h ^= (Double.doubleToLongBits(this.e()) >>> 32) ^ Double.doubleToLongBits(this.e());\n"
+                + "    h *= 1000003;\n"
+                + "    h ^= this.f() ? 1231 : 1237;\n"
+                + "    h *= 1000003;\n"
+                + "    h ^= java.util.Arrays.hashCode(this.g());\n"
+                + "    h *= 1000003;\n"
+                + "    h ^= this.h().hashCode();\n"
+                + "    h *= 1000003;\n"
+                + "    h ^= (i() == null) ? 0 : this.i().hashCode();\n"
+                + "    return h;\n"
+                + "  }\n"
+                + "}"
+    );
+
+    assertAbout(javaSources())
+        .that(Arrays.asList(ignoreHashEquals, nullable, source))
+        .processedWith(new AutoValueProcessor())
+        .compilesWithoutError()
+        .and()
+        .generatesSources(expectedSource);
+  }
+}
